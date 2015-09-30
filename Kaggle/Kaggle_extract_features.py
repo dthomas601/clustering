696cc7460bc6350c2f1578ee119f0a306a06370d
unknown <dmt101@DMT101-PC.hpc.msstate.edu>
2015-09-30 13:29:34 -0500

    Added code to Kaggle_extract_features.py

--- Kaggle/Kaggle_extract_features.py
+++ Kaggle/Kaggle_extract_features.py
@@ -3,7 +3,9 @@ __author__ = 'temp'
 import os
 import sys
 import re
+sys.path.append("C:\\DeMarcus\\cuckoo_parser\\Experimental\\MalwareFeatures")
 import pe_class as p
+import multiprocessing
 
 import string
 from multiprocessing import Pool
@@ -11,12 +13,11 @@ from sklearn.feature_extraction.text import HashingVectorizer
 
 
 
-
 def parse_file(file):
     with open(file,'r') as f:
         line=""
         for l in f:
-            line+=str(" ".join(l.split(" ")[1:])).replace("\r\n"," ")
+            line+=str(" ".join(l.split(" ")[1:])).replace("\n"," ")
         return line
 
 
@@ -28,42 +29,40 @@ def tokenize(text):
 
 
 
-
-
 def getVector(sample_path):
+
     bytes=parse_file(sample_path)
-
     #print bytes
-    #print tokenize(bytes)
-
     vectorizer= HashingVectorizer(analyzer='char',input='content',decode_error='ignore',
-                                 strip_accents='ascii',ngram_range=(2,2),n_features=524288,tokenizer=tokenize)
+                                 strip_accents='ascii',ngram_range=(1,2),n_features=50)#n_features=1048576)#,tokenizer=tokenize)
 
 
 
-    return p.fileName(sample_path),vectorizer.fit_transform(bytes)
+    return p.fileName(sample_path),vectorizer.fit_transform(bytes).toarray()
 
 
 if __name__ == '__main__':
 
-    sys.stdout = open('output_file2.txt', 'w')
+    #sys.stdout = open('output_file2.txt', 'w')
 
     #base_path="/Volumes/malware/samples/Kaggle"
-
-    base_path="/Volumes/DISK_IMG/samples/Kaggle/train"
+    base_path="Z:\\projects\\idaho-bailiff\\C4\\Labeled_Malware_Family_Dataset\\train"
+    #base_path="/Volumes/DISK_IMG/samples/Kaggle/train"
 
 
     sample_list=[]
     bytes_list=[]
 
 
+
     for s in os.listdir(base_path):
         if '.bytes' in s:
             sample_list.append(os.path.join(base_path,s))
 
-    for s in sample_list:
-        print getVector(s)
 
 
+    pool=Pool(maxtasksperchild=25,processes=multiprocessing.cpu_count()-2)
+    for results in pool.imap(getVector,sample_list):
+        print results
 
 
