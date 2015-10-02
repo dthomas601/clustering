d121958fdb6b9f0dda93cf37f0e260d408a225bc
unknown <dmt101@DMT101-PC.hpc.msstate.edu>
2015-10-02 16:33:07 -0500

    Kaggle test file.....ran into memory error on work system.

--- Kaggle/Kaggle_extract_features.py
+++ Kaggle/Kaggle_extract_features.py
@@ -8,8 +8,23 @@ import pe_class as p
 import multiprocessing
 from multiprocessing import Pool
 from sklearn.feature_extraction.text import HashingVectorizer
-from scipy import sparse
+from itertools import repeat
+import csv
 
+
+
+def read_csv(path):
+    with open(path, 'rb') as f:
+        reader = csv.reader(f)
+        mydict={}
+        unique=set()
+        for rows in reader:
+            mydict[rows[0]] = rows[1]
+            if str(rows[1]).isdigit():
+                unique.add(rows[1])
+
+
+    return mydict,sorted(unique)
 
 def parse_file(file):
     with open(file,'r') as f:
@@ -27,10 +42,10 @@ def tokenize(text):
 
 
 
-def getVector(sample_path):
+def getVector((sample_path,dic)):
 
     #print parse_file(sample_path)
-    return parse_file(sample_path)
+    return parse_file(sample_path),dic.get(os.path.basename(sample_path).split(".")[0])
 
 
 if __name__ == '__main__':
@@ -39,9 +54,13 @@ if __name__ == '__main__':
 
     #base_path="/Volumes/malware/samples/Kaggle/train"
     base_path="Z:\\projects\\idaho-bailiff\\C4\\Labeled_Malware_Family_Dataset\\train"
+    label_path="Z:\\projects\\idaho-bailiff\\C4\\Labeled_Malware_Family_Dataset\\trainLabels.csv"
     #base_path="/Volumes/DISK_IMG/samples/Kaggle/train"
 
 
+    labels,unique=read_csv(label_path)
+
+    label_listing=[]
     sample_list=[]
 
     for s in os.listdir(base_path):
@@ -50,15 +69,24 @@ if __name__ == '__main__':
 
     blist=[]
     pool=Pool(maxtasksperchild=25,processes=multiprocessing.cpu_count()-2)
-    for results in pool.imap(getVector,sample_list):
+    #for results in pool.imap(getVector,sample_list):
+    for results in pool.imap(getVector,zip(sample_list,repeat(labels))):
+
         #matrix=sparse.hstack((matrix,results),format='csr')
         #print matrix.shape
-        blist.append(results)
+        #print results[1]
+        blist.append(results[0])
+        label_listing.append(results[1])
+
+
 
     vectorizer= HashingVectorizer(analyzer=str.split,input='content',decode_error='ignore',
                                  strip_accents='ascii',ngram_range=(1,2),n_features=1048576)#,tokenizer=tokenize)
 
     v=vectorizer.fit_transform(blist)
+    print labels
+    print len(unique)
+    print label_listing
 
 
 
